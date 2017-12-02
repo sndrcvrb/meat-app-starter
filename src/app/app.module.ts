e0d1f89c3e5b21bbf01f5a922965e28db6122ab7
Sandro Ribeiro <sandroribeiro@outlook.com.br>
2017-12-02 09:14:12 -0200

    adicionando componentes de detalhes, menu e carrinho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestaurantsService } from 'app/restaurants/restaurants.service';
+import { RestaurantsService } from './restaurants/restaurants.service';
+import { RestaurantDetailComponent } from './restaurants/restaurant/restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurants/restaurant/restaurant-detail/menu/menu.component';
+import { MenuItemComponent } from './restaurants/restaurant/restaurant-detail/menu/menu-item/menu-item.component';
+
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 
 @NgModule({
@@ -22,7 +28,11 @@ import { RestaurantsService } from 'app/restaurants/restaurants.service';
     HomeComponent,
     AboutComponent,
     RestaurantsComponent,
-    RestaurantComponent
+    RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    ShoppingCartComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule,
