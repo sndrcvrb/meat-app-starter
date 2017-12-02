e53c7de02045ed3c2b836ef4cd80087354ac9c78
Sandro Ribeiro <sandroribeiro@outlook.com.br>
2017-12-02 08:04:46 -0200

    tratamento de erro básico na chamada http do serviço de restaurantes

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,11 +1,13 @@
 import { Restaurant } from './restaurant/restaurant.model';
 import { MEAT_API } from '../app.api';
+import { ErrorHandler } from '../app.error-handler';
 
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class RestaurantsService {
@@ -14,7 +16,9 @@ export class RestaurantsService {
   }
 
   restaurants(): Observable<Restaurant[]> {
-    return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json())
+    return this.http.get(`${MEAT_API}/restaurants`)
+    .map(response => response.json())
+    .catch(ErrorHandler.handleError)
   }
 
 }
