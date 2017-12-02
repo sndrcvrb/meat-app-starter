b8c6499d2e7e8c2faab2796730dcc944da7b6094
Sandro Ribeiro <sandroribeiro@outlook.com.br>
2017-12-02 09:51:03 -0200

    carregando dinamicamente os detalhes do restaurante

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,7 +1,7 @@
-import { RestaurantsService } from './restaurants.service';
-import { Restaurant } from './restaurant/restaurant.model';
-
 import { Component, OnInit } from '@angular/core';
+
+import { Restaurant } from './restaurant/restaurant.model';
+import { RestaurantsService } from './restaurants.service';
 
 @Component({
   selector: 'mt-restaurants',
@@ -9,12 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantsComponent implements OnInit {
 
-  restaurants: Restaurant[]
+  restaurants: Restaurant[];
 
   constructor(private restaurantsService: RestaurantsService) { }
 
   ngOnInit() {
-    this.restaurantsService.restaurants().subscribe(restaurants => this.restaurants = restaurants)
+    this.restaurantsService.restaurants().subscribe(restaurants => this.restaurants = restaurants);
   }
 
 }
