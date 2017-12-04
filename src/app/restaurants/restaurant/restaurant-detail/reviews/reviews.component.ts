a9a3027d8f8d7d45b0e68436c0dd4a983f437f4a
Sandro Ribeiro <sandroribeiro@outlook.com.br>
2017-12-04 20:31:16 -0200

    consumindo api de reviews

--- src/app/restaurants/restaurant/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurants/restaurant/restaurant-detail/reviews/reviews.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+import { RestaurantsService } from '../../../restaurants.service';
 
 @Component({
   selector: 'mt-reviews',
   templateUrl: './reviews.component.html'
 })
 export class ReviewsComponent implements OnInit {
+  reviews: Observable<any>;
 
-  constructor() { }
+  constructor(
+    private restaurantsService: RestaurantsService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.reviews = this.restaurantsService.restaurantReviews(this.route.parent.snapshot.params['id']);
   }
-
 }
--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -27,4 +27,10 @@ export class RestaurantsService {
     .catch(ErrorHandler.handleError);
   }
 
+  restaurantReviews(id: string): Observable<any> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+    .map(response => response.json())
+    .catch(ErrorHandler.handleError);
+  }
+
 }
